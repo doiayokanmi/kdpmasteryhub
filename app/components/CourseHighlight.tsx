cd0142b26980030de322cb722bc414d7d40d6373
Damilola Ibrahim <oloyedammy@gmail.com>
2026-02-11 05:50:37 -0800

    mmmmmjjdjjduiiiii7757575

--- app/components/CourseHighlight.tsx
+++ app/components/CourseHighlight.tsx
@@ -19,7 +19,7 @@ const item = {
     show: {
         opacity: 1,
         y: 0,
-        transition: { duration: 0.6, ease: 'easeOut' },
+        transition: { duration: 0.6, ease: 'easeOut' as const },
     },
 }
 
